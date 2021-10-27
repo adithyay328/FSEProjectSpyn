070af5d6e2e2b89aeb11d59d3aba0a321991bd5d
Adithya Yerramsetty <adithyay@hotmail.com>
2021-10-26 18:26:30 -0700

    All of our current work.

---
+++ src/ColorSensing.m
@@ -0,0 +1,58 @@
+import Brick.*;
+import keyboard.*;
+import colorSensor.*;
+import bluetooth.*;
+import wfBrickIO.*;
+import usbBrickIO.*;
+
+%All the different brick connection schemes
+%brick = Brick('ioType','wifi','wfAddr','127.0.0.1','wfPort',5555,'wfSN','0016533dbaf5');
+brick = ConnectBrick("ROCKET")
+%brick = legoev3('Bluetooth','COM6');
+%brick = legoev3("Bluetooth");
+%brick = legoev3("USB");
+
+global key
+InitKeyboard();
+
+myev3 = brick;
+class(brick)
+
+%brick.playTone(100, 800, 500);
+
+%v = myev3.GetBattVoltage();
+
+%colorSens = colorSensor("ROCKET", 3);
+
+brick.SetColorMode(1, 4);
+
+while 2 > 1
+    color_rgb = brick.ColorRGB(1);
+    %fprintf("\tRed %d\n", color_rgb(1));
+    %fprintf("\tGreen %d\n", color_rgb(2));
+    %fprintf("\tBlue %d\n", color_rgb(3));
+    
+    red = color_rgb(1);
+    green = color_rgb(2);
+    blue = color_rgb(3);
+    
+    fprintf("red" + ": %d\n", red);
+    fprintf("green" + ": %d\n", green);
+    fprintf("blue" + ": %d\n", blue);
+    
+    colorList = [red,green,blue];
+    sorted = sort(colorList);
+    
+    highestValue = sorted(2);
+    
+    color = "";
+    if highestValue == red
+        color = "red";
+    elseif highestValue == blue
+        color = "blue";
+    else
+        color = "green";
+    end
+       
+    fprintf(color + ": %d\n", highestValue);
+end
---
+++ src/MatlabKeyboardControlSim.m
@@ -0,0 +1,59 @@
+import Brick.*;
+import keyboard.*;
+import colorSensor.*;
+import bluetooth.*;
+import wfBrickIO.*;
+import usbBrickIO.*;
+
+%brick = Brick('ioType','wifi','wfAddr','127.0.0.1','wfPort',5555,'wfSN','0016533dbaf5');
+%brick = ConnectBrick("ROCKET")
+%brick = legoev3('Bluetooth','COM6');
+%brick = legoev3("Bluetooth");
+%brick = legoev3("USB");
+
+global key
+InitKeyboard();
+
+myev3 = brick;
+class(brick)
+
+%brick.playTone(100, 800, 500);
+
+%v = myev3.GetBattVoltage();
+
+%colorSens = colorSensor('ROCKET', 3);
+
+%disp(v);
+
+leftMotor = motor(myev3, 'A');
+rightMotor = motor(myev3, 'B');
+
+while 1
+    pause(0.1);
+    
+    switch key
+        case 'uparrow'
+            disp("Up Arrow pressed!");
+            %brick.MoveMotor('A', 50);
+            %brick.MoveMotor('B', 50);
+            leftMotor.Speed = 10;
+            rightMotor.Speed = 10;
+        case 'downarrow'
+            disp("Down Arrow Pressed!");
+            brick.MoveMotor('A', -50);
+            brick.MoveMotor('B', -50);
+        case 'leftarrow'
+            disp("Left Arrow Pressed!");
+            brick.MoveMotor('A', 50);
+            brick.MoveMotor('B', -50);
+        case 'rightarrow'
+            disp("Right Arrow Pressed!");
+            brick.MoveMotor('A', -50);
+            brick.MoveMotor('B', 50);
+        case 0
+            disp("No Key Pressed!");
+        case "q"
+            break;
+     end
+end
+CloseKeyboard();
